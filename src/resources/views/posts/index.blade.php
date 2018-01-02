77e0ada3eea6605f218274ef0bf7fce1983483a1
SLEAZOIDS <vegetarian304@gmail.com>
2018-01-02 18:43:38 +0900

    viewの用意

---
+++ src/resources/views/posts/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Blog POST</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Blog POST</h1>
+    <ul>
+      <li><a href="">title</a></li>
+      <li><a href="">title</a></li>
+      <li><a href="">title</a></li>
+    </ul>
+  </div>
+</body>
+
+</html>
+
