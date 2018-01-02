2cc1935902f9f46701f45b7544c2e98d776c26a3
SLEAZOIDS <vegetarian304@gmail.com>
2018-01-02 18:51:52 +0900

    DBからデータ取得

--- src/app/Http/Controllers/PostsController.php
+++ src/app/Http/Controllers/PostsController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 class PostsController extends Controller
 {
     public function index(){
+      $post = Post::latest()->get();
+      dd($post->toArray());
       return view('posts.index');
     }
 }
