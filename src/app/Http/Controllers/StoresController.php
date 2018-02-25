d22dac01ecdcce7caa7737caddb8bb228c4e3cad
SLEAZOIDS <vegetarian304@gmail.com>
2018-02-25 16:58:57 +0900

    API

--- src/app/Http/Controllers/StoresController.php
+++ src/app/Http/Controllers/StoresController.php
@@ -7,6 +7,11 @@ use App\Store;
 
 class StoresController extends Controller
 {
+    public function index()
+    {
+        return response(Store::all());
+    }
+
     public function show(Store $store){
       return view('stores.show')->with('store', $store);
     }
