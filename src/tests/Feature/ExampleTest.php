97b45e8b1f755eb3f08f2036ca5a98b583662134
SLEAZOIDS <vegetarian304@gmail.com>
2018-08-05 00:10:29 +0900

    テストはtest用DBで行う

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -2,11 +2,19 @@
 
 namespace Tests\Feature;
 
+use Artisan;
+use DB;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    public function setUp() {
+        parent::setUp();
+        Artisan::call('migrate:refresh');
+        Artisan::call('db:seed');
+    }
+
     /**
      * A basic test example.
      *
@@ -18,4 +26,10 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testDBTest()
+    {
+        $this->assertEquals('test', DB::connection()->getDatabaseName());
+    }
+
 }
